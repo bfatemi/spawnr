f181305cac57039902acd8fe9042ad029057d26a
Bobby Fatemi <bfatemi07@gmail.com>
2017-01-12 18:18:27 -0800

    Created function to connect to github. Need to create an 'auth' function so this package and my other packages can authenticate me interactively.

--- inst/ext/install_scripts/run_on_server.R
+++ inst/ext/install_scripts/run_on_server.R
@@ -1,6 +1,23 @@
 library(httr)
-url <- 'https://api.github.com/user/keys?access_token='
-ptoke <- '084369bccf2977ee0041c62da91218ed2fbb9cf9'
-pk <- readLines('~/.ssh/authorized_keys')
-pknam <- paste0('server_', Sys.Date(), '_', ceiling(runif(1, 1, 1000)))
-POST(paste0(url, ptoke), body = list(title = pknam, key = pk), encode = 'json')
+
+connect2github <- function(personal_token=NULL){
+  if(is.null(personal_token)){
+    personal_token <- Sys.getenv("GITHUB_PAT")
+    if(personal_token == "")
+      return(FALSE)
+  }
+  api <- 'https://api.github.com/user/keys?access_token='
+  pk <- readLines('~/.ssh/authorized_keys')
+  pknam <- paste0('server_', Sys.Date(), '_', ceiling(runif(1, 1, 1000)))
+  url <- paste0(api, personal_token)
+  r <- POST(url, body = list(title = pknam, key = pk), encode = 'json')
+
+  if(http_error(r))
+    return(FALSE)
+  return(TRUE)
+}
+
+connect2github()
+
+
+
